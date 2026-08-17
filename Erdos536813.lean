3e0be0de5e39350e4f182ad82809b58958aacf20
saralogsdon <91227887+saralogsdon@users.noreply.github.com>
2026-08-16 22:20:01 -0400

    Add import for GoodCoreQuotientFibers

--- Erdos536813.lean
+++ Erdos536813.lean
@@ -37,3 +37,4 @@ import Erdos536813.GoodCoreCountLower
 import Erdos536813.GoodCoreCountUpper
 import Erdos536813.GoodCoreDensityError
 import Erdos536813.GoodCoreDensityLimit
+import Erdos536813.GoodCoreQuotientFibers
