cd18420eb7da8a06d648355db3e71f175df7c6c0
saralogsdon <91227887+saralogsdon@users.noreply.github.com>
2026-08-16 21:20:46 -0400

    Add import for GoodCoreResidueBlocks module

--- Erdos536813.lean
+++ Erdos536813.lean
@@ -29,3 +29,4 @@ import Erdos536813.CanonicalFiveBlock
 import Erdos536813.CombinedFiveBlockWeight
 import Erdos536813.PositiveDepthHighWeight
 import Erdos536813.GoodCoreResidues
+import Erdos536813.GoodCoreResidueBlocks
