b1a7a13488b0bbe12b1d29c4052268b0f05fb959
saralogsdon <91227887+saralogsdon@users.noreply.github.com>
2026-08-17 13:11:36 -0400

    Add BlockAlignedTruncation import to Erdos536813.lean

--- Erdos536813.lean
+++ Erdos536813.lean
@@ -53,3 +53,4 @@ import Erdos536813.HighLayerIntervals
 import Erdos536813.HighLayerPartition
 import Erdos536813.HighLayerUnion
 import Erdos536813.HighLayerUnionInterval
+import Erdos536813.BlockAlignedTruncation
